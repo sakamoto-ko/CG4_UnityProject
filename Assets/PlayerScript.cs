42f016da116aaa1bd6c0215a1602cd793b5541e8
sakamoto-ko <115054832+sakamoto-ko@users.noreply.github.com>
2024-05-08 15:02:27 +0900

    移動にz軸を追加

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -19,6 +19,7 @@ public class PlayerScript : MonoBehaviour
 
         const float moveSpeedX = 5.0f;
         const float moveSpeedY = 8.0f;
+        const float moveSpeedZ = 5.0f;
 
         if (Input.GetKey(KeyCode.D))
         {
@@ -33,9 +34,26 @@ public class PlayerScript : MonoBehaviour
             v.x = 0;
         }
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            v.z = moveSpeedZ;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            v.z = -moveSpeedZ;
+        }
+        else
+        {
+            v.z = 0;
+        }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             v.y = moveSpeedY;
+        }
+        else
+        {
+            v.y = 0;
         }
 
         rb.velocity = v;
